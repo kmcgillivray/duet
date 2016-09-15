15fedcaff19e0705911d4fc8045a3539afa88c96
Kevin McGillivray <kevin.mcgillivray@me.com>
2016-09-14 21:02:36 -0500

    Create sponsored posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,6 +19,17 @@ end
 
 posts = Post.all
 
+50.times do
+  SponsoredPost.create!(
+    topic: topics.sample,
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph,
+    price: RandomData.random_number
+  )
+end
+
+sponsored_posts = SponsoredPost.all
+
 100.times do
   Comment.create!(
     post: posts.sample,
@@ -29,4 +40,5 @@ end
 puts "Seed finished"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
+puts "#{SponsoredPost.count} sponsored posts created"
 puts "#{Comment.count} comments created"
